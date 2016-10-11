829c91413294c7d2997d7c5444b751cac6e179ab
Calvin Szeto <szeto.calvin@gmail.com>
2016-10-11 12:09:34 -0400

    Add basic clip functionality to front end.

--- app/assets/javascripts/directives/video_player/video_player.js
+++ app/assets/javascripts/directives/video_player/video_player.js
@@ -3,12 +3,37 @@ app.directive("videoPlayer", function() {
 		restrict: 'E',
 		templateUrl: 'assets/directives/video_player/video_player.html',
 		controller: ['$scope', 'Videos', '$routeParams', '$sce', function($scope, Videos, $routeParams, $sce) {
+			$scope.setVideoLinkFromClip = function(clip) {
+				var videoLink = $scope.fullVideoLink + '#t=' + clip.start;	
+				if (clip.end) {
+					videoLink += ',' + clip.end;
+				}
+				$scope.videoLink = $sce.trustAsResourceUrl(videoLink);
+			};
+
+			$scope.createNewClip = function() {
+				$scope.videoClips.push($scope.newClip);	
+				$scope.newClip = {};
+			}
+
+			$scope.videoClips = [];
+			// Make the full video the first clip
+			$scope.videoClips.push(
+					{
+						name: 'Full Video',
+						start: '0'
+					}
+			);
+			// Set the current video
 			let videoId = $routeParams.videoId;
 			if(videoId !== undefined) {
 				var video = Videos.get({videoId: videoId}, function(video) {
-					$scope.videoLink = $sce.trustAsResourceUrl(video.video_url);
+					$scope.fullVideoLink = $sce.trustAsResourceUrl(video.video_url);
+					$scope.videoLink = $scope.fullVideoLink;
 				});	
 			}
+
+			$scope.newClip = {};
 		}]
 	}
 })
